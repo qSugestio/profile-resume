c0bbb884568a4fe98ebb692ddfae3216260cde04
qSugestio <danila2014204@gmail.com>
2025-01-20 16:46:33 +0300

    add pet-project column

---
+++ src/components/PPColumn/PP/PP.tsx
@@ -0,0 +1,17 @@
+import styles from './PP.module.css'
+
+const PP = () => {
+  return (
+    <div className={styles.container}>
+      <h6 className={styles.title}>Title</h6>
+      <main className={styles.description}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod id modi
+        quidem commodi aut accusamus, sit, consequuntur tempora a in perferendis
+        tenetur labore cupiditate dolorem delectus voluptate recusandae non
+        aliquid.
+      </main>
+    </div>
+  )
+}
+
+export default PP
--- src/components/PPColumn/PPColumn.tsx
+++ src/components/PPColumn/PPColumn.tsx
@@ -1,9 +1,11 @@
+import PP from './PP/PP'
 import styles from './PPColumn.module.css'
 
 const PPColumn = () => {
   return (
     <div className={styles.pp_container}>
       <h5>Pet-Projects</h5>
+      <PP />
     </div>
   )
 }
