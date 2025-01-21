0cb4445fba3923fcd03d746247050cab99f09ee7
qSugestio <danila2014204@gmail.com>
2025-01-21 15:10:24 +0300

    css refactoring

--- src/components/PPColumn/PP/PP.tsx
+++ src/components/PPColumn/PP/PP.tsx
@@ -1,15 +1,63 @@
+import { useEffect, useState } from 'react'
+import Repo from '../IRepo'
 import styles from './PP.module.css'
 
-const PP = () => {
+interface props {
+  repo: Repo
+}
+interface repoReadmeJson {
+  content?: string
+  status?: string
+  message?: string
+}
+
+const getRepoDescription = async (url: string): Promise<string> => {
+  try {
+    const response = await fetch(`${url}/readme`)
+
+    const json: repoReadmeJson = await response.json()
+    if (json.status === '404' || json.content === undefined)
+      return 'Описание отсутствует'
+
+    let decodedContent = atob(json.content)
+    let utf8Decoder = new TextDecoder('utf-8')
+    let result = utf8Decoder.decode(
+      new Uint8Array(decodedContent.split('').map(char => char.charCodeAt(0)))
+    )
+
+    const regex = /## О проекте([\s\S]*?)##/
+    const description = result.match(regex)?.[1].trim() as string
+    return description
+  } catch (error) {
+    console.error('Error fetching repo description:', error)
+    return 'Описание отсутствует'
+  }
+}
+
+const PP = ({ repo }: props) => {
+  const [description, setDescription] = useState('Описание отсутствует')
+
+  useEffect(() => {
+    const getAndSetDescription = async () => {
+      try {
+        const description = await getRepoDescription(repo.url)
+        setDescription(description)
+      } catch (error) {
+        console.error('Error fetching readme:', error)
+        setDescription('Возникла ошибка при загрузке описания')
+      }
+    }
+
+    // getAndSetDescription()
+  }, [])
+
   return (
     <div className={styles.container}>
-      <h6 className={styles.title}>Title</h6>
-      <main className={styles.description}>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod id modi
-        quidem commodi aut accusamus, sit, consequuntur tempora a in perferendis
-        tenetur labore cupiditate dolorem delectus voluptate recusandae non
-        aliquid.
-      </main>
+      <h6 className={styles.title}>{repo.name}</h6>
+      <main className={styles.description}>{description}</main>
+      <a className={styles.github_link} href={repo.html_url}>
+        [github]
+      </a>
     </div>
   )
 }
--- src/components/PPColumn/PPColumn.tsx
+++ src/components/PPColumn/PPColumn.tsx
@@ -1,11 +1,45 @@
+import { useEffect, useState } from 'react'
+import Repo from './IRepo'
 import PP from './PP/PP'
 import styles from './PPColumn.module.css'
 
+const getRepositories = async (): Promise<Repo[]> => {
+  const response = await fetch('https://api.github.com/users/qSugestio/repos')
+  const repos = await response.json()
+  return repos
+}
+
 const PPColumn = () => {
+  const [repos, setRepos] = useState<Repo[]>([])
+
+  useEffect(() => {
+    const getAndSetRepos = async () => {
+      try {
+        const repos = await getRepositories()
+        setRepos(repos)
+      } catch (error) {
+        console.error('Error fetching repos:', error)
+        setRepos([
+          {
+            name: 'Репозитории не найдены',
+            description: '¯\\_(ツ)_/¯',
+            html_url: '',
+            language: '',
+            url: '',
+          },
+        ])
+      }
+    }
+
+    getAndSetRepos()
+  }, [])
+
   return (
     <div className={styles.pp_container}>
       <h5>Pet-Projects</h5>
-      <PP />
+      {repos.map((repo: Repo) => (
+        <PP repo={repo} key={repo.name} />
+      ))}
     </div>
   )
 }
