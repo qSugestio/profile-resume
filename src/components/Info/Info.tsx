e8ac2c8ef8ffc39176b1d081615cbf9d9f2f4e11
qSugestio <danila2014204@gmail.com>
2025-03-27 15:15:31 +0300

    add skills

--- src/components/Info/Info.tsx
+++ src/components/Info/Info.tsx
@@ -6,7 +6,38 @@ const Info = () => {
         <img src='/avatar.svg' alt='avatar' />
       </div>
       <p className={styles.nickname}>Sugestio</p>
-      <span className={styles.bio}>Важная инфа обо мне ^_^</span>
+      <span className={styles.bio}>
+        <span>Данила Князев</span>
+        <span>Fullstack-разработчик</span>
+        <span>
+          Навыки:
+          <ul>
+            Frontend:
+            <li>JS</li>
+            <li>TS</li>
+            <li>React.js</li>
+            <li>React router</li>
+          </ul>
+          <ul>
+            <li>Redux</li>
+            <li>Next.js</li>
+            <li>axios</li>
+          </ul>
+          <br />
+          <ul>
+            Backend:
+            <li>Nest.js</li>
+            <li>Node.js</li>
+            <li>Express.js</li>
+            <li>JWT</li>
+          </ul>
+          <ul>
+            <li>Postgresql</li>
+            <li>REST API</li>
+            <li>Socket.io</li>
+          </ul>
+        </span>
+      </span>
     </div>
   )
 }
